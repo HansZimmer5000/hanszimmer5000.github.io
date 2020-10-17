d7bc17915445db66bec87733d03d0689ea8ff8bb
HansZimmer <michael.weitnau@live.de>
2020-10-17 21:37:47 +0200

    add todos

--- pieces/build.sh
+++ pieces/build.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+final_dir=".."
+index_site="publications.html"
+
+# TODO consider all pages 
+# TODO copy page
+# TODO input template and rename to respective page
+# TODO insert input from commons and the respective page
+
+for page in $(ls pages); do
+    echo $page
+done
+
